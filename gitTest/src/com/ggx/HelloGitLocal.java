826c758629f12cf1a1df13a887a93428099fa94b
Ggxiao <ggxiao111@163.com>
2016-12-16 11:51:22 +0800

    idea local add 新建新文件

---
+++ gitTest/src/com/ggx/HelloGitLocal.java
@@ -0,0 +1,12 @@
+package src.com.ggx;
+
+/**
+ * Created by guoguangxiao on 16/12/16.
+ */
+public class HelloGitLocal {
+
+    public static void main(String[] args){
+        System.out.println("idea本地master分支提交");
+    }
+
+}
